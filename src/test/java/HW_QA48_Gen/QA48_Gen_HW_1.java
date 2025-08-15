dc55e05ddfe960c0cd6bbe3ade2351574236b400
genpo <genpolinam@gmail.com>
2025-08-15 10:13:52 +0300

    exp

---
+++ src/test/java/HW_QA48_Gen/QA48_Gen_HW_1.java
@@ -0,0 +1,24 @@
+package HW_QA48_Gen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.json.JsonOutput;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class QA48_Gen_HW_1 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://ed.kidskey.org/");
+    }
+
+    @Test
+    public void openKidskeyTest(){
+        System.out.println("Kidskey opened");
+    }
+}
+
