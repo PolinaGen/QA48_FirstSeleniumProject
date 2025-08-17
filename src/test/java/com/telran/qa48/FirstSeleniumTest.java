7b7b0a5bc89af866403c0cfed9bef591997407e5
genpo <genpolinam@gmail.com>
2025-08-17 19:32:45 +0300

    new

--- src/test/java/com/telran/qa48/FirstSeleniumTest.java
+++ src/test/java/com/telran/qa48/FirstSeleniumTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class FirstSeleniumTest {
 
     WebDriver driver;
@@ -13,7 +15,18 @@ public class FirstSeleniumTest {
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        //максимамальный размер экрана
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.tel-ran.com"); // with history
+        //ожидание всех элементов загрузки на сайте до теста
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // driver.get("https://www.google.com"); // without history
+        driver.navigate().to("https://www.google.com");
+
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();//аппликации, где хранятся персональные данные!
     }
 
     //test -name
